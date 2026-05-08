bb89f9665eba9d1b5061bf4b3b43de000455dcd7
Ramiro <videoanalisisracing2022@gmail.com>
2026-05-08 11:48:17 -0300

    arreglando un mal pegado

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import * as XLSX from "xlsx";
 import emailjs from "@emailjs/browser";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wjbmjauvwyseugtaugjb.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "PEGAR_ACA_TU_PUBLISHABLE_KEYsb_publishable_exynDYj6j9ZtXOwhiAC7vQ_dD5Swvu2";
+const SUPABASE_URL = "https://wjbmjauvwyseugtaugjb.supabase.co/";
+const SUPABASE_ANON_KEY = "sb_publishable_exynDYj6j9ZtXOwhiAC7vQ_dD5Swvu2";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
